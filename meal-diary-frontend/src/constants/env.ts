d3772f399e2bb9fcfd38dff5c00f46382cb5c0e4
Janne Lavila <Janne555@users.noreply.github.com>
2020-06-24 21:27:53 +0300

    update stuff to use env vars

---
+++ meal-diary-frontend/src/constants/env.ts
@@ -0,0 +1,25 @@
+const envVarNames = [
+  "REACT_APP_AUTH0_CLIENT_ID",
+  "REACT_APP_AUTH0_DOMAIN",
+  "REACT_APP_AUTH0_AUDIENCE"
+] as const
+
+type EnvVarName = (typeof envVarNames)[number]
+
+const env = (() => {
+  return envVarNames.reduce(
+    (env, name): Record<EnvVarName, string> => ({ ...env, [name]: getEnvOrThrow(name) }),
+    {} as Record<EnvVarName, string>
+  )
+})()
+
+function getEnvOrThrow(name: EnvVarName): string {
+  const val = process.env[name]
+  if (val) {
+    return val
+  } else {
+    throw Error(`Missing env variable ${name}`)
+  }
+}
+
+export default env
