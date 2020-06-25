cfeaa9eae08f4a747364f2c0a84e9ec2ad6e36c3
Janne Lavila <Janne555@users.noreply.github.com>
2020-06-25 11:16:04 +0300

    update env vars

--- meal-diary-backend/src/constants/env.ts
+++ meal-diary-backend/src/constants/env.ts
@@ -4,23 +4,29 @@ const envVarNames = [
   "AUTH0_CLIENT_SECRET",
   "MONGO_URI",
   "APP_NAME",
-  "AUTH0_AUDIENCE",
-  "AUTH0_USER_AUDIENCE",
-  "NODE_ENV",
-  "RUN_FINELI_TASK_ON_START",
+  "AUDIENCE",
   "PORT",
   "AUTH_STRATEGY"
 ] as const
 
+
 type EnvVarName = (typeof envVarNames)[number]
+
+
+const defaults: Partial<Record<EnvVarName, string>> = {
+  PORT: "4000"
+}
 
 const env = (() => {
   const entries = envVarNames
-    .map(envVarName => [envVarName, process.env[envVarName]])
-    .filter((entry): entry is [EnvVarName, string] => {
-      const [name, envVar] = entry
-      if (envVar != null) {
-        return true
+    .map((envVarName): [EnvVarName, string] => {
+      const value = process.env[envVarName]
+      const defaultValue = defaults[envVarName]
+
+      if (value) {
+        return [envVarName, value]
+      } else if (defaultValue) {
+        return [envVarName, defaultValue]
       } else {
         throw Error(`Missing env variable ${name}`)
       }
--- meal-diary-frontend/src/constants/env.ts
+++ meal-diary-frontend/src/constants/env.ts
@@ -1,7 +1,7 @@
 const envVarNames = [
   "REACT_APP_AUTH0_CLIENT_ID",
   "REACT_APP_AUTH0_DOMAIN",
-  "REACT_APP_AUTH0_AUDIENCE"
+  "REACT_APP_AUDIENCE"
 ] as const
 
 type EnvVarName = (typeof envVarNames)[number]
