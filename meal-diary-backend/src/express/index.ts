d60a1be563bb659811aa3904b9284d33e60e5e0f
Janne Lavila <Janne555@users.noreply.github.com>
2020-06-26 01:50:09 +0300

    add session stuff

--- meal-diary-backend/src/express/index.ts
+++ meal-diary-backend/src/express/index.ts
@@ -4,13 +4,27 @@ import { authenticationRoutes } from '../routes'
 import passport from 'passport'
 import { auth0Strategy, auth0JWTStrategy } from '../authentication'
 import { env } from '../constants'
+import session from 'express-session'
+import bodyParser from 'body-parser'
 
 passport.use(auth0Strategy)
 passport.use("auth0-jwt", auth0JWTStrategy)
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
 const app = express()
 
+app.use(session({ secret: Math.random().toString(), saveUninitialized: false, resave: false, cookie: { secure: true } }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(passport.initialize())
+app.use(passport.session());
 app.use("/", authenticationRoutes)
 
 app.use("/graphql", (req, res, next) => {
@@ -18,7 +32,6 @@ app.use("/graphql", (req, res, next) => {
     if (user) {
       req.user = user
     }
-
     next()
   })(req, res, next)
 })
