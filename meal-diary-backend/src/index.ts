f523b4c5d39045f628f4bb74250bb509cb3dde73
Janne Lavila <Janne555@users.noreply.github.com>
2020-06-26 01:49:24 +0300

    use secure server if pem files exist

--- meal-diary-backend/src/index.ts
+++ meal-diary-backend/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { env } from './constants'
 import { app } from './express'
+import https from 'https'
+import fs from 'fs'
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -10,11 +12,21 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect(`${env.MONGO_URI.replace("<dbname>", env.APP_NAME)}`)
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
   console.log("mongodb connection established")
 })
 
-const listener = app.listen(Number(env.PORT), () => {
-  console.log(`🚀 Server listening at ${JSON.stringify(listener.address())}`)
-})
+if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
+  const key = fs.readFileSync('./key.pem')
+  const cert = fs.readFileSync('./cert.pem')
+  const server = https.createServer({ key, cert }, app)
+
+  const listener = server.listen(Number(env.PORT), () => {
+    console.log(`🚀 Server listening at ${JSON.stringify(listener.address())}`)
+  })
+} else {
+  const listener = app.listen(Number(env.PORT), () => {
+    console.log(`🚀 Server listening at ${JSON.stringify(listener.address())}`)
+  })
+}
